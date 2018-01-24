208421843a07545921e094ecba56c91747b32e48
Susanna Lepola <suskaari@hotmail.com>
2018-01-24 14:56:41 +0200

    index content 2

--- pages/index.vue
+++ pages/index.vue
@@ -22,11 +22,11 @@
       <div class="column columnSecond">
         <h1>Welcome To My Portfolio</h1>
       </div>
-      <div class="column">
+      <div class="column columnThird">
         <h1>About me</h1>
-        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-        <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-        <p>I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
       </div>
       <div class="column">
         <h1>Experience</h1>
@@ -59,9 +59,13 @@ export default {
 }
 .columnThird {
   background-color:bisque;
+  padding: 50px;
 }
 .columnSlogan {
   padding: 50px 0 50px 0;
   margin: 0;
+}
+.indexParagraph {
+  padding: 10px 0 10px 0;
 }
 </style>
