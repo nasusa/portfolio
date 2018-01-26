4566680c00da05c7271f31c94eedd5e4e5d8035c
Susanna Lepola <suskaari@hotmail.com>
2018-01-26 13:36:20 +0200

    columns

--- pages/index.vue
+++ pages/index.vue
@@ -1,55 +1,40 @@
 <template>
-  <section>
-    <section class="section">
-      <div class="container">
-          <h1>I am Susanna</h1>
+  <section>      
+    <section>  
+      <div class="headerContent has-text-centered">
+        <ul>
+          <li><span class="headerFont">I am Susanna</span></li>
+          <li><span class="headerSloganFont"> DESIGNER <i class="fa fa-angle-double-right" aria-hidden="true"></i> ICT TECHNICIAN <i class="fa fa-angle-double-right" aria-hidden="true"></i> DREAMER </span></li>
+          <li><span class="headerFont">Welcome to my Portfolio</span> </li>
+        </ul>
       </div>
-    </section>
+      </section>
 
-    <section class="section">
-      <div class="container">
-        <div class="columns columnSlogan is-mobile has-text-centered">
-          <div class="column is-one-third">
-            <hr>
-            <hr>
-          </div>
-          <div class="column is-one-third is-uppercase">
-            <p>Designer</p>
-            <p>Ict-technician<p>
-            <p>Dreamer</p>
-          </div>
-          <div class="column is-one-third">
-            <hr>
-            <hr>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="section">
-      <div class="container">
-        <h1>Welcome To My Portfolio</h1>
-      </div>
-    </section>
-
-    <section class="section">
-      <div class="container">
-        <h1>About me</h1>
-        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-        <p class="indexParagraph">I'm a paragraph. Click here to add your own text and edit me. I’m a great place for you to tell a story and let your users know a little more about you.</p>
-      </div>
-    </section>
-
-    <section class="section">
-      <div class="container">
-          <h1>Experience</h1>
-          <p>Here are my skills</p>
-      </div>
-    </section>
-
-    <section class="section">
+      <section class="section">
         <div class="container">
+          <columns class="columns columnsHero">
+            <column class="column columnDesign has-text-centered">
+            yks
+            </column>
+            <column class="column columnProject has-text-centered">
+            kaks
+            </column>
+            <column class="column columnCamera has-text-centered">
+            kolme
+            </column>
+          </columns>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container has-text-centered">
+            <h1>Experience</h1>
+            <p>Here are my skills</p>
+        </div>
+      </section>
+
+      <section class="section">
+        <div class="container has-text-centered">
           <h1>Skills</h1>
           <p>Here are my skills</p>
         </div>
@@ -68,6 +53,60 @@ export default {
 </script>
 
 <style>
+.headerContent {
+  height: 450px;
+  font-size: 3em;
+  display: -webkit-flex;
+  align-items: center;
+  -webkit-justify-content: center;
+  background-image: url("~/static/susa.jpg");
+  position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center 40%;
+  z-index: -1;
+}
+.headerFont {
+    font-size: 1em;
+    color: rgba(48,48,48,1);
+    text-shadow: 1px 1px #FFFAF0;
+    opacity: 0.6;
+    font-family: 'Sacramento', cursive;
+  }
+.headerSloganFont {
+    font-size: 0.5em;
+    color:  #cccccc;
+    letter-spacing: 3px;
+    font-weight: bold;
+    font-family: 'Abel', sans-serif;
+  }
+.columnsHero {
+  margin-top: -5em;
+  background-color: white;
+  padding: 40px;
+  border-radius: 15px;
+}
+.columnDesign {
+  height: 150px;
+  width: 150px;
+  background-image: url("~/static/design.jpg");
+  position: relative;
+  background-size: cover;
+}
+.columnProject {
+  height: 150px;
+  width: 150px;
+  background-image: url("~/static/project.jpg");
+  position: relative;
+  background-size: cover;
+}
+.columnCamera {
+  height: 150px;
+  width: 150px;
+  background-image: url("~/static/camera.jpg");
+  position: relative;
+  background-size: cover;
+}
 .indexParagraph {
   padding: 10px 0 10px 0;
 }
