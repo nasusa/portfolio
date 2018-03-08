a9fb879f6a2d353d19f328e9182892cd3bc2065c
Susanna Lepola <suskaari@hotmail.com>
2018-03-08 10:39:13 +0200

    peli

---
+++ pages/peliportaali.vue
@@ -0,0 +1,116 @@
+<template>
+    <section>
+        <section class="hero is-info is-fullheight">
+            <div class="hero-head">
+                <nav class="navbar">
+                    <div class="container">
+                        <div class="navbar-brand">
+                            <a class="navbar-item">
+                                <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+                            </a>
+                            <span class="navbar-burger burger" data-target="navbarMenuHeroB">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                        </div>
+                        <div id="navbarMenuHeroB" class="navbar-menu">
+                            <div class="navbar-end">
+                                <a class="navbar-item is-active">
+                                Peliportaali
+                                </a>
+                                <a class="navbar-item">
+                                In English
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                <p class="title">
+                    Title
+                </p>
+                <p class="subtitle">
+                    Subtitle
+                </p>
+                </div>
+            </div>
+
+            <div class="hero-foot">
+                <nav class="tabs is-boxed is-fullwidth">
+                <div class="container">
+                    <ul>
+                        <li class="is-active">
+                            <a>Fyysisen kuntoutuksen pelit</a>
+                        </li>
+                        <li>
+                            <a>Kognitiivisen kuntoutuksen pelit</a>
+                        </li>
+                        <li>
+                            <a>Opetuspelit</a>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+            </div>
+        </section>
+        
+        <section class="hero">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns is-desktop has-text-centered is-uppercase">
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                    </div>
+                    <div class="columns is-desktop has-text-centered is-uppercase">
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                        <div class="column">
+                            <img class="game" src="~/static/1.png">
+                            <h1>Lorem ipsum</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+</template>
+
+<script>
+  export default {
+    layout: 'blank'
+    }
+</script>
+
+<style lang="scss">
+    .game {
+        height: 80%;
+    }
+</style>
--- pages/peliprojekti.vue
+++ pages/peliprojekti.vue
@@ -1,100 +1,144 @@
 <template>
-<section>
-    <section class="mainHero hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container">
-            <h1 class="title">
-                Lorem ipsum Game
-            </h1>
-            <h2 class="subtitle">
-                dolor sit amet, Fusce feugiat malesuada
-            </h2>
-            </div>
-        </div>
-    </section>
-    <section class="hero is-medium">
-        <div class="hero-body">
-            <div class="container">
-                <div class="levelColumns columns has-text-centered">
-                    <div class="column is-one-third">
-                        <h1 class="is-uppercase is-size-4">Lorem ipsum dolor sit</h1>
-                            <h2 class="imageContent">um ciis natoque tib</h2>
-                        <img class="levelImage" src="~/static/1.jpg">
-                    </div>
-                    <div class="column">
-                        <h1 class="is-uppercase is-size-4">Fusce feugiat malesuada</h1>
-                        <h2 class="imageContent">ulrulla dugi malea iog</h2>
-                        <img class="levelImage" src="~/static/2.jpg">
-                    </div>
-                    <div class="column">
-                        <h1 class="is-uppercase is-size-4">Consectetuer adipiscing</h1>
-                        <h2 class="imageContent">sems ultrice pharet</h2>
-                        <img class="levelImage" src="~/static/3.jpg">
-                    </div>
+    <section>
+        <nav class="navbar is-transparent">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://bulma.io">
+                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                </a>
+                <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+                <span></span>
+                <span></span>
+                <span></span>
                 </div>
             </div>
-        </div>
-    </section>
-    <section class="hero is-primary">
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns infoColumns">
-                    <div class="column is-half infoColumn">
-                        <h1 class="title is-uppercase is-size-3">Tervetuloa</h1>
-                        <h2 class="is-uppercase is-size-5">Ultriceharetra gnec accuan mal suada</h2>
-                        <p>Sugimale ada odi dio gravida atcur ecuus a lorem. Maeenas tristique orc ac semuis ultriceharetra gnec accuan male suadaorcnec sit amet 
-                            eroorem ipsum dol. or sit amt consc tetuer aiing elit. Mauris fermentum tumagna.
-                        </p>
-                        <figure class="image is-16by9">
-                            <img class="videoImage" src="~/static/trailer.png">
-                        </figure>    
-                    </div>
-                    <div class="column">
-                        <h1 class="title is-uppercase is-size-3">Ketä olemme?</h1>
-                        <div class="columns">
-                            <div class="column is-one-fifth">
-                                <img class="profile" src="~/static/portrate1.jpeg">
-                            </div>
-                            <div class="column">
-                                <h1 class="is-uppercase">Scrum master</h1>
-                                <p>Sugimale ada odi dio gravida atcur ecuus a lorem. Maeenas tristique orc ac semuis ultriceharetra gnec accuan male suada.
-                                </p>
-                                <p>aabbcc@cs.tamk.fi</p>
-                            </div>
-                        </div> 
-                        <div class="columns">
-                            <div class="column is-one-fifth">
-                                <img class="profile" src="~static/portrate2.jpeg">
-                            </div>
-                            <div class="column">
-                                <h1 class="is-uppercase">Koodari</h1>
-                                <p>Maeenas tristique orc ac semuis ultriceharetra gnec accum male suada orcnec sit amet eroorem ipsum dolor sit amt consc tet.
-                                </p>
-                                <p>aabbcc@cs.tamk.fi</p>
-                            </div>
+
+            <div id="navbarExampleTransparentExample" class="navbar-menu">
+                <div class="navbar-start is-uppercase">
+                    <a class="navbar-item" href="#">
+                        Yhteistyössä
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Peli
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Kehitystiimi
+                    </a>
+                    <a class="navbar-item" href="#">
+                        Peliportaali
+                    </a>
+                    <a class="navbar-item" href="#">
+                        In English
+                    </a>
+                </div>
+            </div>
+        </nav>
+
+        <section class="mainHero hero is-fullheight">
+            <div class="hero-body">
+                <div class="container">
+                <h1 class="title">
+                    Lorem ipsum Game
+                </h1>
+                <h2 class="subtitle">
+                    dolor sit amet, Fusce feugiat malesuada
+                </h2>
+                </div>
+            </div>
+        </section>
+
+        <section class="hero is-medium">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="levelColumns columns has-text-centered">
+                        <div class="column is-one-third">
+                            <h1 class="is-uppercase is-size-4">Lorem ipsum dolor sit</h1>
+                                <h2 class="imageContent">um ciis natoque tib</h2>
+                            <img class="levelImage" src="~/static/1.png">
                         </div>
-                        <div class="columns">
-                            <div class="column is-one-fifth">
-                                <img class="profile" src="~static/portrate3.jpeg">
-                            </div>
-                            <div class="column">
-                                <h1 class="is-uppercase">Koodari</h1>
-                                <p>Ut te dolor dapibus eget elentu vel cursus eleifend enean auctor wrn liqua volutpat. Duis ac turpisteger rutrum ante eu lacu.
-                                </p>
-                                <p>aabbcc@cs.tamk.fi</p>
-                            </div>
+                        <div class="column">
+                            <h1 class="is-uppercase is-size-4">Fusce feugiat malesuada</h1>
+                            <h2 class="imageContent">ulrulla dugi malea iog</h2>
+                            <img class="levelImage" src="~/static/1.png">
+                        </div>
+                        <div class="column">
+                            <h1 class="is-uppercase is-size-4">Consectetuer adipiscing</h1>
+                            <h2 class="imageContent">sems ultrice pharet</h2>
+                            <img class="levelImage" src="~/static/1.png">
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
+        <section class="hero infoHero">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns infoColumns">
+                        <div class="column is-half is-desktop infoColumn">
+                            <h1 class="title is-half-desktop is-uppercase is-size-3">Tervetuloa</h1>
+                            <h2 class="is-uppercase is-size-5">Ultriceharetra gnec accuan mal suada</h2>
+                            <p>Sugimale ada odi dio gravida atcur ecuus a lorem. Maeenas tristique orc ac semuis ultriceharetra gnec accuan male suadaorcnec sit amet 
+                                eroorem ipsum dol. or sit amt consc tetuer aiing elit. Mauris fermentum tumagna.
+                            </p>
+                            <img class="videoImage" src="~/static/video.jpg">
+                        </div>
+                        <div class="column">
+                            <h1 class="title is-uppercase is-size-3">Kehitystiimi</h1>
+                            <div class="columns">
+                                <div class="column is-one-fifth">
+                                    <img class="profile" src="~/static/people.png">
+                                </div>
+                                <div class="column">
+                                    <h1 class="is-uppercase">Scrum master</h1>
+                                    <p>Sugimale ada odi dio gravida atcur ecuus a lorem. Maeenas tristique orc ac semuis ultriceharetra gnec accuan male suada.
+                                    </p>
+                                    <p>aabbcc@cs.tamk.fi</p>
+                                </div>
+                            </div> 
+                            <div class="columns">
+                                <div class="column is-one-fifth">
+                                    <img class="profile" src="~static/people.png">
+                                </div>
+                                <div class="column">
+                                    <h1 class="is-uppercase">Koodari</h1>
+                                    <p>Maeenas tristique orc ac semuis ultriceharetra gnec accum male suada orcnec sit amet eroorem ipsum dolor sit amt consc tet.
+                                    </p>
+                                    <p>aabbcc@cs.tamk.fi</p>
+                                </div>
+                            </div>
+                            <div class="columns">
+                                <div class="column is-one-fifth">
+                                    <img class="profile" src="~static/people.png">
+                                </div>
+                                <div class="column">
+                                    <h1 class="is-uppercase">Koodari</h1>
+                                    <p>Ut te dolor dapibus eget elentu vel cursus eleifend enean auctor wrn liqua volutpat. Duis ac turpisteger rutrum ante eu lacu.
+                                    </p>
+                                    <p>aabbcc@cs.tamk.fi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="content has-text-centered">
+                    <p>
+                        <strong>@Potku Productions.</strong> Kaikki oikeudet pidätetään.
+                    </p>
+                </div>
+            </div>
+        </footer>
     </section>
-</section>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    layout: 'blank'
+    }
 </script>
 
 <style lang="scss">
@@ -102,7 +146,7 @@ export default {
     position: relative;
     background-size: cover;
     background-repeat: no-repeat;
-    background-image: url("~/static/gamebg.jpg");
+    background-image: url("~/static/header.jpg");
 }
 .levelColumns{
     .levelImage {
@@ -117,15 +161,22 @@ export default {
     border-radius: 50%;
 }
 .infoColumns {
-    padding: 0px 50px;
+    @include desktop {
+        padding: 0px 50px;
+    }
 }
 .infoColumn {
-    padding-right: 8em;
+    @include desktop {
+        padding-right: 8em;
+    }
 }
 .videoImage {
     padding: 30px 30px 10px 0px;
 }
 .imageContent {
     padding-bottom: 20px;
+}
+.infoHero {
+    background-color: white;
 }
 </style>
